5a3908a424a3a5e3f6fa7cde788ad984ae421957
Klemen Skok <klemen.skok1@gmail.com>
2024-11-28 00:16:18 +0100

    28-11-2024, lahko noc

--- js/main.js
+++ js/main.js
@@ -1,2 +1,9 @@
+
+window.addEventListener('load', () => {
+    const preloader = document.getElementById('preloader');
+    preloader.style.display = 'none';
+})
+
+
 document.getElementsByTagName('center')[0].innerHTML += '<h2>Hello, World!</h2>';
 document.getElementsByTagName('center')[0].innerHTML += '<h2>Ne ni še konc</h2>';
