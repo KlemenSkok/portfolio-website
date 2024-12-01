4e1fcafe6ed9233e84053e34031400856b4dafb2
Klemen Skok <klemen.skok1@gmail.com>
2024-12-02 00:03:32 +0100

    02-12-2024 ukročen je začetni prikaz strani in navbar. mrbit bi dodal se puscico ki kaze navzdol proti vsebini strani. todo: uredi stil sekcij, uposoti je treba kartice od projektov (preberes iz data/projects.json)

--- js/main.js
+++ js/main.js
@@ -2,15 +2,4 @@
 window.addEventListener('load', () => {
     document.getElementById('preloader').style.display = 'none';
     document.getElementById('navbar').style.display = 'flex';
-})
-
-// ne dela
-document.querySelectorAll('nav-icon').forEach(icon => {
-    icon.addEventListener('mouseover', () => {
-        //let parent = icon.parentElement;
-        //let icon_text = parent.querySelector('.icon-text');
-        let icon_text = icon.nextElementSibling;
-        icon_text.style.display = 'block';
-        console.log('called');
-    });
 });
