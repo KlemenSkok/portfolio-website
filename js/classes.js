44d8e9a3530fdd0e76be44bff92781ae68cb64c1
Klemen Skok <klemen.skok1@gmail.com>
2024-12-06 00:12:24 +0100

    05-12-2024 skillsi so konc, generiranje projekctov in skillsov je blo prestavljen v external classa v js/classes.js. dodan delujoc gumb za togglanje color modea (light/dark). todo: color mode v localStorage

--- js/classes.js
+++ js/classes.js
@@ -2,16 +2,30 @@
 function getRandomColor() {
     // hsl format: hsl(x, 40%, 85%)
     let x = Math.floor(Math.random() * 360);
-    return `hsl(${x}, 40%, 85%)`;
+    return `hsl(${x}, 30%, 30%)`;
 }
 
-class Skills {
-    colors = new Map();
+function shuffleArray(array) { // da so skilli nakljucno razporejeni
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1)); // Random index from 0 to i
+        [array[i], array[j]] = [array[j], array[i]];  // Swap elements
+    }
+    return array;
+}
+
+// loadanje skillov
+export class Skills {
+    colors;
     data;
 
+    constructor () {
+        this.colors = new Map();
+        this.data = [];
+    }
 
-    constructor (path) {
-        fetch(path)
+    async load (path) {
+        console.log('loading skills data');
+        await fetch(path) // ! await, da pocaka na fetch
         .then(response => {
             if(!response.ok) {
                 throw new Error('Failed to open skills.json');
@@ -19,7 +33,9 @@ class Skills {
             return response.json();
         })
         .then(data => {
-            this.data = data;
+            this.data = data.skills;
+            this.data = shuffleArray(this.data);
+            //console.log(this.data);
         })
         .catch(err =>{
             console.log('Error loading skills data: ', err);
@@ -32,9 +48,91 @@ class Skills {
             if(!this.colors.has(s.type)) {
                 this.colors.set(s.type, getRandomColor());
             }
-        });  
+        });
     }
+    
+    generate() { // add to html container
+        const root = document.getElementById('skills-container');
+        this.data.forEach(s => {
+            let box = document.createElement('div');
+            box.setAttribute('class', 'skill-box');
+            box.style.borderColor = this.colors.get(s.type);
+            box.innerHTML = `${s.name}`;
+            root.appendChild(box);
+        });
+    }
+    // end of class
+}
+// loadanje projektov
+export class Projects {
+    data;
+
+    async load(path) {
+        await fetch('https://raw.githubusercontent.com/KlemenSkok/portfolio-website/main/data/projects.json')
+        .then(response =>  {
+            if(!response.ok) {
+                throw new Error('Failed to open projects.json');
+            }
+            //console.log(response.json());
+            return response.json();
+        })
+        .then(data => { // data gets output of response.json()
+            this.data = data.projects;
+        })
+        .catch(err => {
+            console.log('Error loading projects data: ', err);
+        });
+    }
+
     generate() {
-        // add to html container
+        const root = document.getElementById('projects-ul');
+        this.data.forEach(p => {
+            const li = document.createElement('li');
+            let descr = "";
+            p.description.forEach(d => {
+                descr += `<p>${d}</p>`;
+            });
+
+            li.innerHTML = `
+                <div class="project-card">
+                    <div class="project-card-header">
+                        <div class="project-title"><h1>${p.name}</h1></div>
+                        <div class="drop-arrow">
+                            <span class="material-symbols-outlined arrow-up-down">keyboard_arrow_down</span>
+                        </div>
+                    </div>
+                    <div class="project-card-body">
+                        <div class="project-description">${descr}</div>
+                        <div class="project-footer">
+                            <div class="project-status"><b>Status:</b> <span>${p.status}.</span></div>
+                            <div class="project-link"><a href="${p.link}"><i>${p.link_text}</i></a></div>
+                        </div>
+                        <div class="project-img"><img src="${p.image}" alt="${p.image_alt}" width="60%"/></div>
+                    </div>
+                </div>
+            `;
+            root.appendChild(li);
+        });
     }
+
+    // vsem project karticam doda event listenerje, da se expandajo ko jih kliknes
+    setEventListeners() {
+        // returns HTMLcollection, not array!
+        const projectHeaders = document.getElementsByClassName('project-card-header');
+
+        //console.log(projectHeaders.length);
+        Array.from(projectHeaders).forEach(header => {
+            header.addEventListener('click', () => {
+                const body = header.nextElementSibling;
+                body.classList.toggle('open');
+                let arrow = header.querySelector('.drop-arrow').querySelector('.arrow-up-down').innerHTML;
+                if(arrow === 'keyboard_arrow_down') {
+                    header.querySelector('.drop-arrow').querySelector('.arrow-up-down').innerHTML = 'keyboard_arrow_up';
+                } else {
+                    header.querySelector('.drop-arrow').querySelector('.arrow-up-down').innerHTML = 'keyboard_arrow_down';
+                }
+            });
+        });
+    }
+    // end of class
 }
--- js/main.js
+++ js/main.js
@@ -1,113 +1,72 @@
+import { Projects, Skills } from  './classes.js';
 
-// vsem karticam doda event listenerje, da se expandajo ko jih kliknes
-function setProjectCards() {
-    // returns HTMLcollection, not array
-    const projectHeaders = document.getElementsByClassName('project-card-header');
 
-    //console.log(projectHeaders.length);
-    Array.from(projectHeaders).forEach(header => {
-        header.addEventListener('click', () => {
-            const body = header.nextElementSibling;
-            body.classList.toggle('open');
-            let arrow = header.querySelector('.drop-arrow').querySelector('.arrow-up-down').innerHTML;
-            if(arrow === 'keyboard_arrow_down') {
-                header.querySelector('.drop-arrow').querySelector('.arrow-up-down').innerHTML = 'keyboard_arrow_up';
-            } else {
-                header.querySelector('.drop-arrow').querySelector('.arrow-up-down').innerHTML = 'keyboard_arrow_down';
+async function readProjectsData() {
+    const projects = new Projects();
+    try {
+        await projects.load('https://raw.githubusercontent.com/KlemenSkok/portfolio-website/main/data/projects.json');
+
+        projects.generate();
+        projects.setEventListeners();
+    }
+    catch(err) {
+        console.log('Error loading projects data: ', err);
+    }
+}
+
+async function readSkillsData() {
+    const skills = new Skills();
+    try {
+        await skills.load('https://raw.githubusercontent.com/KlemenSkok/portfolio-website/main/data/skills.json')
+        
+        // ti dve metodi pocakata prvo
+        skills.classify();
+        skills.generate();
+    }
+    catch(err) {
+        console.log('Error loading skills data: ', err);
+    }
+}
+
+function enableSmoothScroll() {
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+    
+            const target = document.querySelector(this.getAttribute('href'));
+            if (target) {
+                target.scrollIntoView({
+                    behavior: 'smooth', // Smooth scroll
+                    block: 'start'      // Align to the top
+                });
             }
         });
     });
 }
 
-function fillProjects(data) {
+function toggleColorMode() {
+    const sw = document.getElementById('color-switcher');
 
-    const projects = data.projects;
-    //console.log(projects);
-
-    const root = document.getElementById('projects-ul');
-    projects.forEach(p => {
-        const li = document.createElement('li');
-        let descr = "";
-        p.description.forEach(d => {
-            descr += `<p>${d}</p>`;
-        });
-
-        li.innerHTML = `
-            <div class="project-card">
-                <div class="project-card-header">
-                    <div class="project-title"><h1>${p.name}</h1></div>
-                    <div class="drop-arrow">
-                        <span class="material-symbols-outlined arrow-up-down">keyboard_arrow_down</span>
-                    </div>
-                </div>
-                <div class="project-card-body">
-                    <div class="project-description">${descr}</div>
-                    <div class="project-footer">
-                        <div class="project-status"><b>Status:</b> <span>${p.status}.</span></div>
-                        <div class="project-link"><a href="${p.link}"><i>${p.link_text}</i></a></div>
-                    </div>
-                    <div class="project-img"><img src="${p.image}" alt="${p.image_alt}" width="60%"/></div>
-                </div>
-            </div>
-        `;
-        root.appendChild(li);
-    });
-    
-    setProjectCards();
-}
-
-function fillSkills(data) {
-    const skills = data.skills;
-
-    const root = document.getElementById('skills-container');
-
-    skills.forEach(s => {
-        const div = document.createElement('div');
-        div.setAttribute('class', 'skill-box');
-        div.innerHTML = `${s.name}`;
-        root.appendChild(div);
-    });
-}
-
-function readProjectsData() {
-    fetch('https://raw.githubusercontent.com/KlemenSkok/portfolio-website/main/data/projects.json')
-    .then(response =>  {
-        if(!response.ok) {
-            throw new Error('Failed to open projects.json');
-        }
-        //console.log(response.json());
-        return response.json();
-    })
-    .then(data => { // data gets output of response.json()
-        fillProjects(data);
-    })
-    .catch(err => {
-        console.log('Error loading projects data: ', err);
-    });
-}
-
-function readSkillsData() {
-    fetch('https://raw.githubusercontent.com/KlemenSkok/portfolio-website/main/data/skills.json')
-    .then(response => {
-        if(!response.ok) {
-            throw new Error('Failed to open skills.json');
-        }
-        return response.json();
-    })
-    .then(data => {
-        fillSkills(data);
-    })
-    .catch(err =>{
-        console.log('Error loading skills data: ', err);
-    });
+    if(sw.firstElementChild.innerHTML == 'dark_mode') {
+        sw.firstElementChild.innerHTML = 'light_mode';
+        sw.style.color = '#d9773b';
+        document.getElementById('color-mode').href = 'css/light.css';
+    }
+    else {
+        sw.firstElementChild.innerHTML = 'dark_mode';
+        sw.style.color = 'yellow';
+        document.getElementById('color-mode').href = 'css/dark.css';
+    }
 }
 
 
 window.addEventListener('load', () => {
     readProjectsData();
     readSkillsData();
+    enableSmoothScroll();
     document.getElementById('preloader').style.display = 'none';
     document.getElementById('navbar').style.display = 'flex';
+    document.getElementById('color-switcher').addEventListener('click', toggleColorMode);
 });
 
 
